fcb4cae1d2ed61e0583f14985a2a1f9c211fa204
nicenaito <nicenaito@gmail.com>
2024-03-14 14:38:10 +0900

    ユーザ情報変更はedit_profileからprofileに修正

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def register():
 
 from forms import EditProfileForm
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm(current_user.username)
@@ -85,11 +85,11 @@ def edit_profile():
         current_user.email = form.email.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
+        return redirect(url_for('profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    return render_template('edit_profile.html', title='Edit Profile', form=form)
+    return render_template('profile.html', title='Edit Profile', form=form)
 
 @app.route('/logout')
 @login_required
